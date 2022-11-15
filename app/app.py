ef3e5c21825ba1ae4533284f9085606e8ae1624a
reejith-remanan <reejithremanan@gmail.com>
2022-11-16 02:12:27 +0530

    updation module push

--- app/app.py
+++ app/app.py
@@ -6,33 +6,32 @@ import json
 app = Flask(__name__)
 
 
-def test_table() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'devopsroles'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM test_table')
-    results = [{name: color} for (name, color) in cursor]
-    cursor.close()
-    connection.close()
+config = {
+    'user' : 'ritsoftv4-user' ,
+    'password' : 'ritsoftv4-pass' ,
+    'host' : '117.232.108.35' ,
+    'port' : '8100' ,
+    'database' : 'ritsoftv4'
+}
 
-    return results
+connection = mysql.connector.connect(**config)
+c1 = connection.cursor()
+c2 = connection.cursor()
+
 
 
 @app.route('/')
-#def home():
-#    return render_template('home.html')
-def index() -> str:
-    return json.dumps({'ttable': test_table()})
-
-@app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/attendance')
+def attendance():
+    # id=session['login_id']
+    # c1.execute ('SELECT * FROM mrd_course')
+    # s =  c1.fetchall()
+    c2.execute('SELECT * FROM mrd_courseplan')
+    batch =  c2.fetchall()
+    return render_template('update_att.html', batch=batch)
 
 @app.route('/success/<name>')
 def success(name):
@@ -54,4 +53,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port='8100', debug=True)
